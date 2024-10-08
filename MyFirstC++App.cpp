90220afbab284fb66de2c5a34e3290ef86a717dd
Reda <155382825+0xKa@users.noreply.github.com>
2024-10-08 04:13:45 +0400

    Add files via upload

---
+++ MyFirstC++App.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+int main()
+{
+      std::cout << " Hello my name is walter hartwell white. ";
+
+      std::cout << "I live at 308 Negra Aroya Lane, Albuquerque, New Mexico, 87104.\n\n";
+
+      std::cout << "n8an8 \n\n" << "quandel dingel here " << "yo\n\n\n";
+
+      std::cout <<"the result of " << "10 + 100 + 1000 + 1 = " << 10 + 100 + 1000 + 1 << "\n\n";
+
+      std::cout << "3 * 3 - 3 * 3 + 1 = " << 3 * 3 - 3 * 3 + 1;
+
+      std::cout << "\n\n\n\n\n";
+    return 0;
+   
+}
